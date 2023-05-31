04a405f8365a759c6ef11f9a1cc82bdc4b761847
nyambe <samuel.ebuka@gmail.com>
2023-05-31 10:10:44 +0000

    Deploying to gh-pages from @ nyambe/vue3-lifecycles@3e4a2833d9e4691a9371d77562b896302e7fd76a 🚀

---
+++ _nuxt/ejemplo.36181a5c.js
@@ -0,0 +1,6 @@
+import{a as t,l as n,m as a,b as s,p as l,e as c,q as m,f as o,o as r}from"./entry.f7683cf8.js";const d=o("p",null," (ver consola) ",-1),E=o("pre",null,`			created EJEMPLO
+			Antes de montar EJEMPLO
+			Antes de desmontar INDEX
+			Componente montado EJEMPLO
+			Desmontado INDEX
+		`,-1),e="Vue 3 Lifecycles: Ejemplo cambio de ruta",i={__name:"ejemplo",setup(u){return t({title:e,ogTitle:e}),n(()=>{console.log("Antes de montar EJEMPLO")}),a(()=>{console.log("Componente montado EJEMPLO")}),s(()=>{console.log("Antes de desmontar EJEMPLO")}),l(()=>{console.log("Desmontado EJEMPLO")}),console.log("created EJEMPLO"),(p,_)=>(r(),c("div",null,[m(" Hola ejemplo cambio de ruta "),d,E]))}};export{i as default};
